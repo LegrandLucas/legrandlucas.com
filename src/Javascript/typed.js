56900aabd799d743aa87304d8722401529997fa2
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-05 21:18:26 +0100

    wip waypoint

--- src/Javascript/typed.js
+++ src/Javascript/typed.js
@@ -10,12 +10,23 @@ const introText = new Typed('.intro__text', {
   typeSpeed: 30,
 });
 
-const wkit = new Typed('.wkit', {
-  strings: ["WE KEEP IN TOUCH."],
-  typeSpeed: 30,
+
+const wkit = new Waypoint({
+  element: document.querySelector('#contact__waypoint'),
+  handler() {
+    const wkit = new Typed('.wkit', {
+      strings: ["WE KEEP IN TOUCH."],
+      typeSpeed: 30,
+    });
+  },
 });
 
-const thankYou = new Typed('.thankYou', {
-  strings: ["THANK YOU FOR YOUR TIME."],
-  typeSpeed: 30,
+const thankyou = new Waypoint({
+  element: document.querySelector('#contact__waypoint'),
+  handler() {
+    const thankYou = new Typed('.thankYou', {
+      strings: ["THANK YOU FOR YOUR TIME."],
+      typeSpeed: 30,
+    });
+  },
 });
