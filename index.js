6437f6f555082a3cc921d8147b36a74d17837ecf
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-05 16:54:52 +0100

    wip express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use('/cssFiles', express.static(`${__dirname}./src`));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, './src') });
+});
+
+app.listen(3000, () => {
+  console.log('Listening at port 3000');
+});
