d91ccc5b553c0277e2e7cdcc51454804cd6175ce
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-06 22:11:11 +0100

    wip

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// const express = require('express');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// const path = require('path');
-
-// const app = express();
-
-// app.use('/cssFiles', express.static(`${__dirname}./src`));
-
-// app.get('/', (req, res) => {
-//   res.sendFile('index.html', { root: path.join(__dirname, './src') });
-// });
-
-// app.listen(3000, () => {
-//   console.log('Listening at port 3000');
-// });
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
