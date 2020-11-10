ada33a55c24ee49b79f31c2120818ed1bedc9f39
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-10 17:40:41 +0100

    wip

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('src/views/pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/src/views/pages/index.html`);
+});
+
+// css
+app.use(express.static(`${__dirname}/src/`));
+// scripts
+app.use(express.static(`${__dirname}/src/Javascript/`));
+
+app.listen(port, () => {
+  console.log(`Example app is listening on port http://localhost:${port}`);
+});
