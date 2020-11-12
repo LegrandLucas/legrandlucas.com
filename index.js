e9328156b33522da41ac159ddafa6772b7df5864
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-12 09:45:32 +0100

    nodes modules done

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('src/views/pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
 const express = require('express');
 
 const app = express();
@@ -20,9 +9,10 @@ app.get('/', (req, res) => {
 
 // public
 app.use(express.static('./src/public'));
+app.use(express.static('./src/assets'));
 app.use(express.static('./node_modules/animejs/lib'));
-// app.use(express.static('./node_modules/aniemjs/lib'));
-// app.use(express.static('./node_modules/aniemjs/lib'));
+app.use(express.static('./node_modules/typed.js/lib'));
+app.use(express.static('./node_modules/waypoints/lib'));
 
 app.listen(port, () => {
   console.log(`Example app is listening on port http://localhost:${port}`);
