c3cb304c355f5bcbbd3f2235b86afcee205e7854
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-13 17:59:26 +0100

    fix script + mobile

--- src/public/Javascript/anime.js
+++ src/public/Javascript/anime.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line no-unused-expressions
   anime({
@@ -33,28 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
     targets: '.say-hi__emoji',
     rotate: '1turn',
     duration: 3000,
-  }),
-
-  anime({
-    targets: '.moving__arrow__right',
-    keyframes: [
-      { translateX: -100 },
-      { translateX: 0 },
-    ],
-    duration: 3000,
-    easing: 'easeInBounce',
-    loop: true,
-  });
-
-  anime({
-    targets: '.moving__arrow__left',
-    keyframes: [
-      { translateX: 100 },
-      { translateX: 0 },
-    ],
-    duration: 3000,
-    easing: 'easeInBounce',
-    loop: true,
   });
 
   const contactLinks = new Waypoint({
@@ -129,3 +106,27 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 });
+
+if (window.innerWidth > 540) {
+  anime({
+    targets: '.moving__arrow__right',
+    keyframes: [
+      { translateX: -100 },
+      { translateX: 0 },
+    ],
+    duration: 3000,
+    easing: 'easeInBounce',
+    loop: true,
+  });
+
+  anime({
+    targets: '.moving__arrow__left',
+    keyframes: [
+      { translateX: 100 },
+      { translateX: 0 },
+    ],
+    duration: 3000,
+    easing: 'easeInBounce',
+    loop: true,
+  });
+}
