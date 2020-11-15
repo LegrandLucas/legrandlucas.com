38c0c820a075ac5b18d9c30356f079eba5b32cb9
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-15 18:03:51 +0100

    update

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/src/views/pages/index.html`);
-  res.sendFile(`${__dirname}/src/assets`);
-});
-
-// public
-app.use(express.static('./src/public'));
-app.use(express.static('./src/assets'));
+// // public
+app.use(express.static('./public'));
+// app.use(express.static('./src/assets'));
 
 app.use(express.static('./node_modules/animejs/lib'));
 app.use(express.static('./node_modules/typed.js/lib'));
 app.use(express.static('./node_modules/waypoints/lib'));
 app.use(express.static('./node_modules/aos/dist'));
+// app.use(express.static(__dirname + '/src'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/views/index.html`);
+});
 
 app.listen(port, () => {
   console.log(`Example app is listening on port http://localhost:${port}`);
