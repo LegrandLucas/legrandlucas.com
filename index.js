1fdac9cf1f104d118453306b94229116626f5d81
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-17 18:02:48 +0100

    wip

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('*', (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') res.redirect(`https://legrandlucas.com${req.url}`);
+  else next();
+});
+
 // // public
 app.use(express.static('./public'));
-// app.use(express.static('./src/assets'));
 
 app.use(express.static('./node_modules/animejs/lib'));
 app.use(express.static('./node_modules/typed.js/lib'));
 app.use(express.static('./node_modules/waypoints/lib'));
 app.use(express.static('./node_modules/aos/dist'));
-// app.use(express.static(__dirname + '/src'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/views/index.html`);
-});
-
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') res.redirect(`https://${req.header('host')}${req.url}`);
-  else next();
 });
 
 app.listen(port, () => {
